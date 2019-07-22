8277ae9aa262ba44266bbc6beb5644d487b3e5b6
47 <bendestiny1289@gmail.com>
2019-07-22 17:33:02 +0300

    add js toggle effect

---
+++ js/script.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+ $("#hide").click(function() {
+   $("#hide").toggle();
+   $("#hide1").toggle();
+ });
+});
+$(document).ready(function() {
+ $("#hide1").click(function() {
+   $("#hide").toggle();
+   $("#hide1").toggle();
+ });
+});
+$(document).ready(function() {
+    $("#dev").click(function() {
+      $("#dev").toggle();
+      $("#dev1").toggle();
+    });
+   });
+$(document).ready(function() {
+  $("#dev1").click(function() {
+      $("#dev").toggle();
+      $("#dev1").toggle();
+  });
+});
+$(document).ready(function(){
+  $('#prod').click(function(){
+    $('#prod').toggle();
+    $('#prod1').toggle();
+  });
+});
+$(document).ready(function() {
+  $("#prod1").click(function() {
+      $("#prod").toggle();
+      $("#prod1").toggle();
+  });
+});
