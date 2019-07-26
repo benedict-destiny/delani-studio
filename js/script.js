3152fddd030068720454f5f31cd4c28c374641ca
47 <bendestiny1289@gmail.com>
2019-07-26 06:49:31 +0300

    add hover hide and show effects on portfolio images

--- js/script.js
+++ js/script.js
@@ -34,3 +34,59 @@ $(document).ready(function() {
       $("#prod1").toggle();
   });
 });
+$(document).ready(function(){
+  $("#overlayDark").mouseover(function(){
+    $("#darktext").show();
+  }).mouseout(function(){
+    $("#darktext").hide();
+  });
+});
+$(document).ready(function(){
+  $("#overlay2").mouseover(function(){
+    $("#darktext2").show();
+  }).mouseout(function(){
+    $("#darktext2").hide();
+  });
+});
+$(document).ready(function(){
+  $("#overlay3").mouseover(function(){
+    $("#darktext3").show();
+  }).mouseout(function(){
+    $("#darktext3").hide();
+  });
+});
+$(document).ready(function(){
+  $("#overlay4").mouseover(function(){
+    $("#darktext4").show();
+  }).mouseout(function(){
+    $("#darktext4").hide();
+  });
+});
+$(document).ready(function(){
+  $("#overlay5").mouseover(function(){
+    $("#darktext5").show();
+  }).mouseout(function(){
+    $("#darktext5").hide();
+  });
+});
+$(document).ready(function(){
+  $("#overlay6").mouseover(function(){
+    $("#darktext6").show();
+  }).mouseout(function(){
+    $("#darktext6").hide();
+  });
+});
+$(document).ready(function(){
+  $("#overlay7").mouseover(function(){
+    $("#darktext7").show();
+  }).mouseout(function(){
+    $("#darktext7").hide();
+  });
+});
+$(document).ready(function(){
+  $("#overlay8").mouseover(function(){
+    $("#darktext8").show();
+  }).mouseout(function(){
+    $("#darktext8").hide();
+  });
+});
